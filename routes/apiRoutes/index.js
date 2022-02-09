e3903d73b33e9083fcf95bab4b022fb83daa46e2
wyork <w.york63@gmail.com>
2022-02-09 18:56:21 -0500

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
 
 module.exports = router;
 
